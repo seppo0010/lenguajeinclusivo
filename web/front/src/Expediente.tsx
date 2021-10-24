40a4de7d3be5cf9c398e42ecacb6f7a5ecdc51d0
Sebastian Waisbrot <seppo0010@gmail.com>
2021-10-24 18:00:36 -0300

    filter actuaciones by firmante
    fix #5

--- web/front/src/Expediente.tsx
+++ web/front/src/Expediente.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchExpediente, selectExpediente } from './expedienteSlice'
 import type { RootState } from './store'
@@ -7,20 +7,47 @@ function Expediente({id}: {id: string}) {
   const dispatch = useDispatch()
   const expediente = useSelector(selectExpediente)
   const expedienteStatus  = useSelector((state: RootState) => state.expediente.status)
+  const [firmantesFilter, setFirmantesFilter] = useState('')
+
+  const firmantes = expediente?.actuaciones
+    .map((a) => a.firmantes)
+    .filter((x, y, arr) => arr.indexOf(x) === y)
+    .sort()
 
   useEffect(() => {
     if (expedienteStatus === 'idle') {
       dispatch(fetchExpediente({id}))
     }
   }, [expedienteStatus, dispatch, id])
+
+  const actuaciones = expediente?.actuaciones.filter(
+    (a) => a.firmantes === firmantesFilter || firmantesFilter === ''
+  ) || []
+
   return (
     <div>
-      <p>Expediente: {expediente?.ficha.caratula}</p>
-      {expediente?.actuaciones && <ul>
-        {expediente?.actuaciones.map((act) => <li key={act.actId}>
-          {act.titulo} - {act.firmantes} ({new Date(act.fechaFirma).toString()})
-        </li>)}
-      </ul>}
+      {expediente &&
+      <>
+        <p>Expediente: {expediente?.ficha.caratula}</p>
+        <p>
+          <label>
+            Firmantes:
+            <select onChange={(e) => setFirmantesFilter(e.target.value)}>
+              <option value="">Todos</option>
+              {firmantes?.map((f) => (
+                <option key={f} value={f}>
+                  {f}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
+        {actuaciones && <ul>
+          {actuaciones.map((act) => <li key={act.actId}>
+            {act.titulo} - {act.firmantes} ({new Date(act.fechaFirma).toString()})
+          </li>)}
+        </ul>}
+      </>}
     </div>
   );
 }

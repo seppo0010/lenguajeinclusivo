f9c8d0eeb4193e2dd27ec17564db0675d25a2e9c
Sebastian Waisbrot <seppo0010@gmail.com>
2021-10-24 22:59:29 -0300

    Read PDF images

--- extracttext/main.go
+++ extracttext/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/olivere/elastic/v7"
 	log "github.com/sirupsen/logrus"
@@ -41,11 +43,8 @@ func writeToTempFile(r io.Reader) (string, string, error) {
 	}
 	return dir, p, nil
 }
-func getDocumentText(r io.Reader) (string, error) {
-	_, p, err := writeToTempFile(r)
-	if err != nil {
-		return "", err
-	}
+
+func getDocumentPlainText(p string) (string, error) {
 	cmd := exec.Command("pdftotext", p, "-")
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
@@ -87,6 +86,129 @@ func getDocumentText(r io.Reader) (string, error) {
 		return "", err
 	}
 	return string(stdoutBytes), nil
+}
+
+func pdftohtml(p string) error {
+	cmd := exec.Command("pdftohtml", p)
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to pipe pdftohtml")
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		stderrBytes, _ := ioutil.ReadAll(stderr)
+		log.WithFields(log.Fields{
+			"stderr": string(stderrBytes),
+			"error":  err.Error(),
+		}).Error("failed to run pdftohtml")
+		return err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to wait for pdftohtml")
+		return err
+	}
+	return nil
+}
+
+func readImageText(filename string) (string, error) {
+	cmd := exec.Command("tesseract", "-l", "spa", filename, "-")
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to pipe tesseract")
+		return "", err
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to pipe tesseract")
+		return "", err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		stderrBytes, _ := ioutil.ReadAll(stderr)
+		log.WithFields(log.Fields{
+			"stderr": string(stderrBytes),
+			"error":  err.Error(),
+		}).Error("failed to run tesseract")
+		return "", err
+	}
+
+	stdoutBytes, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to read tesseract's output")
+		return "", err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Error("failed to wait for tesseract")
+		return "", err
+	}
+	return string(stdoutBytes), nil
+}
+
+func getDocumentImagesText(dir, p string) (string, error) {
+	log.WithFields(log.Fields{
+		"directory": dir,
+		"path":      p,
+	}).Info("getting pdf images")
+	err := pdftohtml(p)
+	if err != nil {
+		return "", err
+	}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+	text := ""
+	for _, file := range files {
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".jpg") || strings.HasSuffix(filename, ".png") {
+			log.WithFields(log.Fields{
+				"image": filename,
+			}).Info("reading image text")
+			t, err := readImageText(path.Join(dir, filename))
+			if err != nil {
+				continue
+			}
+			text = fmt.Sprintf("%s\n%s", text, t)
+		}
+	}
+	return text, nil
+}
+
+func getDocumentText(r io.Reader) (string, error) {
+	dir, p, err := writeToTempFile(r)
+	defer os.RemoveAll(dir)
+	if err != nil {
+		return "", err
+	}
+	log.WithFields(log.Fields{}).Info("getting pdf plain text")
+	text, err := getDocumentPlainText(p)
+	if err != nil {
+		return "", err
+	}
+	log.WithFields(log.Fields{}).Info("getting pdf images text")
+	text2, err := getDocumentImagesText(dir, p)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s\n%s", text, text2), nil
 }
 func documentHasText(es *elastic.Client, url string) (bool, error) {
 	res, err := es.Search(shared.DocumentType).
@@ -140,7 +262,40 @@ func updateActuacionWithText(es *elastic.Client, url, text string) error {
 	}
 	return nil
 }
+
+func readFiles(paths []string) {
+	log.WithFields(log.Fields{
+		"files": paths,
+	}).Info("reading files")
+	for _, arg := range paths {
+		reader, err := os.Open(arg)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":    err.Error(),
+				"filename": arg,
+			}).Error("error reading document text")
+			continue
+		}
+		text, err := getDocumentText(reader)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error":    err.Error(),
+				"filename": arg,
+			}).Error("error reading document text")
+			continue
+		}
+		log.WithFields(log.Fields{
+			"filename": arg,
+			"text":     text,
+		}).Print("text")
+	}
+}
+
 func main() {
+	if len(os.Args) > 1 {
+		readFiles(os.Args[1:])
+		return
+	}
 	bucketName := "pdfs"
 	minioClient, err := shared.GetMinioClient(bucketName)
 	if err != nil {

2249c9fada0575ef479288d9271402c5e0bacd61
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-10 17:02:56 -0300

    Fix build paths

--- run.sh
+++ run.sh
@@ -2,19 +2,18 @@
 set -Eeux
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-mkdir -p ${SCRIPT_DIR}/web/public/data
+mkdir -p /tmp/juscaba/pdfs
+mkdir -p public/data
 for ((i=1; i<=$#; i++))
 do
     exp=${!i}
     exp_filename=${!i/\//-}
 
-    ./builder -blacklist=/tmp/juscaba/blacklist "-json=/tmp/juscaba/${exp_filename}.json" -pdfs=./pdfs "-expediente=${exp}" -images=true
+    ./builder "-json=public/data/${exp_filename}.json" -pdfs=/tmp/juscaba/pdfs "-expediente=${exp}" -images=false "-blacklist=${BLACKLIST_REGEX:-}"
 
     pushd ts
-    yarn run ts-node create-index.ts /tmp/juscaba/${exp_filename}.json /tmp/juscaba/${exp_filename}-index.json
+    yarn run ts-node create-index.ts public/data/${exp_filename}.json public/data/${exp_filename}-index.json
     popd
-
-    cp /tmp/juscaba/${exp_filename}{-index,}.json ${SCRIPT_DIR}/web/public/data
 done
 
 yarn
